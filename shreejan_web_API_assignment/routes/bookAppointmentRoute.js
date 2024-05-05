23de29ca44684c0a3482f468d9e5c63944969abf
shreejanmdr <shreejan0497@gmail.com>
2024-05-05 13:52:45 +0545

    edited

--- shreejan_web_API_assignment/routes/bookAppointmentRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const bookAppointmentController = require("../controllers/bookAppointmentController");
-
-router.post("/book-appointment", bookAppointmentController.bookAppointment);
-
-module.exports = router;
--- shreejan_web_API_assignment/routes/reservationRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const reservationController = require("../controllers/reservationController");
-
-router.post("/reservations", reservationController.createReservation);
-
-module.exports = router;
